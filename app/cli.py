3066b2d3d9f5d0746ac87c499c8220ef5ddf262e
ArturLukianov <artur.lukyanov@gmail.com>
2020-04-30 13:13:54 +0300

    Click registration added

--- app/cli.py
+++ app/cli.py
@@ -1,5 +1,22 @@
 import os
 import click
+from app import db
+from app.models import User
+
 
 def register(app):
-    pass
+    @app.cli.group()
+    def user():
+        """User profiles interaction"""
+        pass
+
+    @user.command()
+    @click.argument("username")
+    @click.argument("password")
+    @click.argument("role")
+    def create(username, password, role):
+        new_user = User(username=username, role=int(role))
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        click.echo('User created', color='green')
