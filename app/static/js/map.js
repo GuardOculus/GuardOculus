3123f2f758fcb6c7b7e212fa58c0471ee25bb28d
ArturLukianov <artur.lukyanov@gmail.com>
2020-05-04 14:38:28 +0300

    Autoresize canvas

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,3 +1,30 @@
+function draw() {
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+var sidebarState = 0;
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.canvas.width = window.innerWidth - 400;
+ctx.canvas.height = window.innerHeight - 300;
+
+draw()
+
+$('#canvas').on('switchSidebar', function () {
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+	if(sidebarState == 0)
+	{
+		ctx.canvas.width = window.innerWidth - 200;
+		sidebarState = 1;
+	}
+	else
+	{
+		ctx.canvas.width = window.innerWidth - 400;
+		sidebarState = 0;
+	}
+	draw();
+});
